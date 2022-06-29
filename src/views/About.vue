a3624699549ecbfa62e5f044f7270f9e67783d67
497363983 <497363953@qq.com>
2022-06-29 20:42:32 +0800

    Delete useless code

--- src/views/About.vue
+++ src/views/About.vue
@@ -9,32 +9,10 @@
 // import { runPythonByMain } from "../utils/useIPC";
 // import { request } from "../api/request";
 import { getReference } from "../api/reference";
-// import axios from "axios";
 export default {
   methods: {
     test: function() {
-      // runPythonByMain("./py/hello.py", {
-      //   args:[11,2,3]
-      // }, (err, results) => {
-      //   console.log(err, results);
-      //   console.log(JSON.parse(results[0].replace(/'/g, `"`)));
-      // });
-      // axios
-      //   .get("http://api.crossref.org/works/10.1186/s13045-020-00910-5")
-      //   .then(res => {
-      //     console.log(res);
-      //   });
-      // request.get({
-      //   url: "http://api.crossref.org/works/10.1186/s13045-020-00910-5",
-      //   success: res => {
-      //     console.log(res);
-      //   },
-      //   failure: err => {
-      //     console.error(err);
-      //   }
-      // });
-      // console.log(getReference("10.1016/j.molcel.2018.01.003"))
-      getReference({doi:"references"});
+      getReference({doi:"10.1128/AEM.01156-08"});
     }
   }
 };
