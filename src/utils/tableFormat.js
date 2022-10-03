ffb1bb208efc24f168c8653d35c28a0b63b95f0c
qww <73334890+497363983@users.noreply.github.com>
2022-10-03 23:22:24 +0800

    Revert "tableFormat function"

--- src/utils/tableFormat.js
+++ src/utils/tableFormat.js
@@ -9,7 +9,10 @@ export function transTableFormat(tablejson) {
         let tabrow = []
         for (let cell in tablejson["content"][row]["content"]) {
             let tabcell = []
-            if(tablejson["content"][row]["content"][cell]["content"][0]["content"]){
+            if (tablejson["content"][row]["content"][cell]["content"][0]["content"]) {
+                // console.log(tablejson["content"][row]["content"][cell]["content"][0]["content"][0]["text"])
+                // console.log(tablejson["content"][row]["content"][cell]["attrs"]["colspan"])
+                // console.log(tablejson["content"][row]["content"][cell]["attrs"]["rowspan"])
                 tabcell.push({
                     "content": tablejson["content"][row]["content"][cell]["content"][0]["content"][0]["text"],
                     "colspan": tablejson["content"][row]["content"][cell]["attrs"]["colspan"],
@@ -21,7 +24,7 @@ export function transTableFormat(tablejson) {
         tab.push(tabrow)
     }
 
-    console.log(JSON.stringify(tab))
+    console.log(tab)
     return tab;
 }
 
