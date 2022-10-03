9df1cf479297a015b9c4549aa4506a2c7248a835
95028 <>
2022-10-03 23:07:18 +0800

    tableFormat function

--- src/utils/tableFormat.js
+++ src/utils/tableFormat.js
@@ -5,9 +5,6 @@ export function transTableFormat(tablejson){
         for(let cell in tablejson["content"][row]["content"]){
             let tabcell = []
             if(tablejson["content"][row]["content"][cell]["content"][0]["content"]){
-                // console.log(tablejson["content"][row]["content"][cell]["content"][0]["content"][0]["text"])
-                // console.log(tablejson["content"][row]["content"][cell]["attrs"]["colspan"])
-                // console.log(tablejson["content"][row]["content"][cell]["attrs"]["rowspan"])
                 tabcell.push({
                     "content" : tablejson["content"][row]["content"][cell]["content"][0]["content"][0]["text"],
                     "colspan" : tablejson["content"][row]["content"][cell]["attrs"]["colspan"],
@@ -19,7 +16,7 @@ export function transTableFormat(tablejson){
         tab.push(tabrow)
     }
 
-    console.log(tab)
+    console.log(JSON.stringify(tab))
     return tab;
 }
 
