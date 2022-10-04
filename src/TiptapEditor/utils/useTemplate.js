8e31131f9db4e8f17d034be8f40a8eac710e6cec
sky1 <497363953@qq.com>
2022-10-04 11:41:33 +0800

    upload changes

--- src/TiptapEditor/utils/useTemplate.js
+++ src/TiptapEditor/utils/useTemplate.js
@@ -1,6 +1,7 @@
 import { useTemplateStore } from "@/store";
 import templates from '../templates/index.json';
 import { getDirTree, readFile } from "@/utils/files";
+const path = require('path');
 
 export const entityMap = {
     "&": "amp",
@@ -39,13 +40,17 @@ export const escapeHtml = (str) => String(str).replace(/[&<>"'/\\]/g, (s) => `&$
 
 export function getTemplates() {
     if (useTemplateStore().componentsPath && useTemplateStore().componentsPath.trim() !== '') {
-
+        const templateFiles = getDirTree(useTemplateStore().componentsPath);
+        templateFiles.forEach(file=>{
+            const template = readFile(path.join(useTemplateStore().componentsPath, file));
+            
+        })
     }
 }
 
 
-export function DOMcreateElement(type, attrs, content = null) {
-    
+export function DOMcreateElement(DOM) {
+    const { type, content = null, attrs } = DOM;
 }
 
 export function hasContent() {
--- src/utils/files.js
+++ src/utils/files.js
@@ -1,5 +1,6 @@
 const fs = window.require('fs');
 const path = window.require("path");
+
 /**
  * 
  * @param {String} dirPath 
@@ -28,7 +29,7 @@ export function getDirTree(dirPath, depth = 0, except = ["node_modules", "dist",
                 }
                 if (state.isDirectory()) {
                     item.type = "directory";
-                    item.leaf = isLeaf(subPath)
+                    item.leaf = isLeaf(subPath);
                     item.name = path.basename(subPath);
                     if (currentDepth < depth) {
                         item.children = []
@@ -52,6 +53,7 @@ export function getDirTree(dirPath, depth = 0, except = ["node_modules", "dist",
 
     return getMap(dirPath, depth);
 }
+
 /**
  * 
  * @param {String} dirPath 
@@ -69,6 +71,7 @@ export function isLeaf(dirPath) {
     })
     return res
 }
+
 /**
  * 
  * @param {String} dirPath 
@@ -78,6 +81,7 @@ export function getFilesName(dirPath) {
     let files = fs.readdirSync(dirPath);
     return files;
 }
+
 /**
  * 
  * @param {String} dirPath 
@@ -97,9 +101,9 @@ export function readFile(dirPath, callback) {
         if (callback) {
             callback(err);
         }
-
     }
 }
+
 /**
  * 
  * @param {String} dirPath 
