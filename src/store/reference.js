1c3d48d0c61d95c48bd536205272e72b867c584c
sky1 <497363953@qq.com>
2022-10-11 14:00:50 +0800

    upload

--- src/store/reference.js
+++ src/store/reference.js
@@ -26,40 +26,51 @@ export const useReferenceStore = defineStore('referenceStore', {
         },
         importReferenceByDOI(doi, callback) {
             let references = this.$state.references;
-            getReference({
-                doi,
-                success: async function ({ reference }) {
-                    references.push(reference);
-                    await pullProject({
-                        success: () => {
-                            writeJSONFile(`${useTemplateStore().getProjectPath}\\tool_box\\references\\${doi.replace("/", "_")}.json`, reference, (data) => {
-                                console.log(data);
-                            });
-                            writeJSONFile(`${useTemplateStore().getProjectPath}\\tool_box\\references\\index.json`, references, (data) => {
-                                console.log(data);
-                            });
-                            pushProject({
-                                commitInformation: `upload references`,
-                                file: [`tool_box\\references\\index.json`, `tool_box\\references\\${doi.replace("/", "_")}.json`]
-                            })
-                            ElMessage({
-                                type: "success",
-                                message: `Import success!`
-                            });
-                            if (callback && typeof callback === 'function') {
-                                callback();
+            const doiList = references.map(item => {
+                return item.doi
+            })
+            if (!doiList.includes(doi)) {
+                getReference({
+                    doi,
+                    success: async function ({ reference }) {
+                        references.push(reference);
+                        await pullProject({
+                            success: () => {
+                                writeJSONFile(`${useTemplateStore().getProjectPath}\\tool_box\\references\\${doi.replace("/", "_")}.json`, reference, (data) => {
+                                    console.log(data);
+                                });
+                                writeJSONFile(`${useTemplateStore().getProjectPath}\\tool_box\\references\\index.json`, references, (data) => {
+                                    console.log(data);
+                                });
+                                pushProject({
+                                    commitInformation: `upload references`,
+                                    file: [`tool_box\\references\\index.json`, `tool_box\\references\\${doi.replace("/", "_")}.json`]
+                                })
+                                ElMessage({
+                                    type: "success",
+                                    message: `Import success!`
+                                });
+                                if (callback && typeof callback === 'function') {
+                                    callback();
+                                }
                             }
-                        }
-                    });
-                },
-                failure: function ({ err }) {
-                    console.log(err);
-                    ElMessage({
-                        type: "error",
-                        message: `Import failure!`
-                    });
-                }
-            });
+                        });
+                    },
+                    failure: function ({ err }) {
+                        console.log(err);
+                        ElMessage({
+                            type: "error",
+                            message: `Import failure!`
+                        });
+                    }
+                });
+            } else {
+                ElMessage({
+                    type: "info",
+                    message: `DOI already exists`
+                });
+            }
+
         },
         saveReferences() {
             writeJSONFile(`${useTemplateStore().getProjectPath}\\tool_box\\references\\index.json`, this.$state.references, (data) => {
